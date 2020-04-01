23dcbb612c24eff2eab5812d891e37e53c12b01f
RitvijSrivastava <ritvijsrivastava99@gmail.com>
2020-04-02 01:17:37 +0530

    Updates

---
+++ precache-manifest.682e09ea7285d71061697c9f6e2c8863.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66204116b752343c9469fe4d4b28d8a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "feadb2cfb5a5d1534ca3",
+    "url": "/portfolio/static/css/main.73aa6422.chunk.css"
+  },
+  {
+    "revision": "bc0e8b1aacfb1656b7bb",
+    "url": "/portfolio/static/js/2.fc5d74cd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.fc5d74cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feadb2cfb5a5d1534ca3",
+    "url": "/portfolio/static/js/main.75ed9e50.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  }
+]);
